058e6f3dd3589f125e24d5ebb60459a474edd54b
Brenda <Beh.00@hotmail.com>
2025-03-25 05:32:46 -0300

    class 7

---
+++ Basic/Soma.py
@@ -0,0 +1,4 @@
+n1 = int(input('Enter a number: '))
+n2 = int(input('Enter outher number: '))
+sum = n1 + n2
+print('The sum between {} and {} is equal to {}'.format(n1, n2, sum))
---
+++ TypePrimitive.py
@@ -0,0 +1,4 @@
+a = input('Enter something: ')
+print(type(a))
+print ('Is it a number?', a.isnumeric())
+print ('Is it a text?', a.isalpha())
